7f963a0b23dff9e5475dfc791bdeb79551a685ac
viraj1607 <merai.viraj@gmail.com>
2025-06-02 19:51:12 -0400

    fund me functionality

---
+++ FundMe.sol
@@ -0,0 +1,48 @@
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import {PriceConverter} from "./PriceConverter.sol";
+
+contract FundMe{
+     using PriceConverter for uint256;
+     uint256 public minUsd=5e18;
+     address[] public funders;
+     mapping(address funder=> uint256 amountFunded) public addressToAmountFunded;
+     address public owner;
+
+     constructor(){
+        owner=msg.sender;
+     }
+
+    function fund() public payable {
+        require(msg.value.getConversionRate() >= minUsd,"Didn't send enoght ETH");  //1e18=1ETH= 1*10**18
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender]=addressToAmountFunded[msg.sender] + msg.value;
+    }
+
+    function withdraw() public onlyOwner{
+        for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        funders = new address[](0);
+        // // transfer
+        // payable(msg.sender).transfer(address(this).balance);
+
+        // // send
+        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        // require(sendSuccess, "Send failed");
+
+        // call
+        (bool callSuccess,) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call failed");
+
+
+    }
+
+    modifier onlyOwner(){
+        require(msg.sender==owner,"only owner can call this function");
+        _;
+    }
+}
